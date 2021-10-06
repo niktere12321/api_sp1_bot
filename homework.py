801e717f54abdf1ccd86a2545c58949e4685f770
Никита <nikterekhoff4242@gmail.com>
2021-10-06 22:21:52 +0300

    api1.0

--- homework.py
+++ homework.py
@@ -1,8 +1,24 @@
+import logging
 import os
 import time
+from logging.handlers import RotatingFileHandler
+
 import requests
 import telegram
 from dotenv import load_dotenv
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(
+    'my_logger.log', maxBytes=50000000, backupCount=5
+)
+logger.addHandler(handler)
 
 
 load_dotenv()
@@ -10,16 +26,14 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -27,12 +41,20 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
-    return homework_statuses.json()
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
+    params = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(URL, params=params, headers=headers)
+        return homework_statuses.json()
+    except Exception as e:
+        logging.exception(f'error {e}')
+        return {}
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
@@ -40,12 +62,19 @@ def main():
 
     while True:
         try:
-            ...
+            logger.debug('Отслеживание статуса запущено')
+            homework_status = get_homeworks(current_timestamp)
+            if homework_status.get('homeworks'):
+                send_message(parse_homework_status(homework_status.get('homeworks')[0]))
+            current_timestamp = homework_status.get('current_date')
+            logger.info('Бот отправил сообщение')
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
+            error_message = f'Бот упал с ошибкой: {e}'
+            logger.error(error_message)
+            bot.send_message(CHAT_ID, error_message)
+            time.sleep(10)
 
 
 if __name__ == '__main__':
