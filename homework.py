6bfcdfe75207ef94e8296a0b7f66f55fc93b729c
Никита <nikterekhoff4242@gmail.com>
2021-10-07 22:15:45 +0300

    api1.0

--- homework.py
+++ homework.py
@@ -1,39 +1,57 @@
+import os
+
+import requests
+from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram.ext import CommandHandler, Updater
 
-TELEGRAM_TOKEN = '2033287598:AAFBPr75b4abzYk0rMSH2Q4ph9mvMmC1U38'
+load_dotenv()
 
-updater = Updater(token=TELEGRAM_TOKEN)
+secret_token = os.getenv('TOKEN')
+
+URL = 'https://api.thecatapi.com/v1/images/search'
 
 
-def say_hi(update, context):
+def get_new_image():
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        print(error)
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    
+    response = response.json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+def new_cat(update, context):
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='Извени Макс нас всех')
+    context.bot.send_photo(chat.id, get_new_image())
 
 
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
-    buttons = ReplyKeyboardMarkup([
-            ['Который час?', 'Определи мой ip'],
-            ['/random_digit']
-        ])
+    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+
     context.bot.send_message(
         chat_id=chat.id,
-        text='Спасибо, что вы включили меня, {}!'.format(name),
-        reply_markup=buttons
-        )
+        text='Привет, {}. Посмотри какого котика я тебе нашел'.format(name),
+        reply_markup=button
+    )
+
+    context.bot.send_photo(chat.id, get_new_image())
+
+def main():
+    updater = Updater(secret_token, use_context=True)
+
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+    updater.start_polling()
+    updater.idle()
 
 
-def danil_pidr(update, context):
-    chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='Данил пидр')
-
-
-updater.dispatcher.add_handler(MessageHandler(Filters.text('Пидор'), danil_pidr))
-
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-
-updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
-
-updater.start_polling()
+if __name__ == '__main__':
+    main() 
